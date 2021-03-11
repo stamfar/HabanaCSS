ad486716552638211208dcb69e6d37fc099db420
mainlyno <69307900+mainlyno@users.noreply.github.com>
2021-03-11 13:36:29 +0100

    Script fungerer.

--- habana.no/javascript/interaction.js
+++ habana.no/javascript/interaction.js
@@ -2,20 +2,21 @@
 
 
 /* Clickable Link Block for hcard(Habana Card) */
-const card = document.querySelector(".hcard");
-const mainLink = card.querySelector(".main-link");
-const clickableElements = Array.from(card.querySelectorAll("clickable"));
+const hcard = document.querySelector(".hcard");
+const hmainLink = hcard.querySelector(".hmain-link");
+const hclickableElements = Array.from(hcard.querySelectorAll("a")); 
+//we are using 'a' here for simplicity but ideally you should put a class like 'clickable' on every clicable element inside hcard(a, button) and use that in query selector.
 
-clickableElements.forEach((ele) =>
-    ele.addEventListener("click", (e) => e.stopPropagation())
+hclickableElements.forEach((ele) =>
+	ele.addEventListener("click", (e) => e.stopPropagation())
 );
 
 function handleClick(event) {
-    const noTextSelected = !window.getSelection().toString();
+	const noTextSelected = !window.getSelection().toString();
 
-    if (noTextSelected) {
-        mainLink.click();
-    }
+	if (noTextSelected) {
+		hmainLink.click();
+	}
 }
 
-card.addEventListener("click", handleClick);
+hcard.addEventListener("click", handleClick);
