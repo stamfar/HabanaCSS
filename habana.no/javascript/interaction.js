3c3f92d6f2fd51b5b7a1a4f8875a1cd9c136964e
mainlyno <69307900+mainlyno@users.noreply.github.com>
2021-03-12 10:42:11 +0100

    tester. funker ok

--- habana.no/javascript/interaction.js
+++ habana.no/javascript/interaction.js
@@ -2,26 +2,7 @@
 
 
 window.onload = function() {
-// Clickable Link Block for hcard(Habana Card)
-const hcard = document.querySelector(".hcard");
-const hmainLink = hcard.querySelector(".hmain-link");
-const hclickableElements = Array.from(hcard.querySelectorAll("a")); 
-//we are using 'a' here for simplicity but ideally you should put a class like 'clickable' on every clicable element inside hcard(a, button) and use that in query selector.
 
-hclickableElements.forEach((ele) =>
-	ele.addEventListener("click", (e) => e.stopPropagation())
-);
-
-function handleClick(event) {
-	const noTextSelected = !window.getSelection().toString();
-
-	if (noTextSelected) {
-		hmainLink.click();
-	}
-}
-
-hcard.addEventListener("click", handleClick);
-}
 
 
 
@@ -98,3 +79,4 @@ $(".card a").hover(
 		$(".statusbar").css("opacity", "1");
 	}
 );
+}
